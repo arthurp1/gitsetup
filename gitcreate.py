92dbffd8cbeee3620796b56f12419edc05bc2c75
arthurp1 <arthur.poot1@gmail.com>
2017-02-02 14:19:06 +0100

    initial commit

--- gitcreate.py
+++ gitcreate.py
@@ -15,10 +15,6 @@ repo = removeNewline(ntpath.basename(current_path))
 #github username
 user = removeNewline(subprocess.check_output('git config user.name', shell = True))
 
-
-# cmd1 
-# hello = "Hello {0} {1}".format(42, 'twenny five')
-
 gitinit = "git init"
 gitadd = "git add ."
 gitcommit = "git commit -m 'initial commit'"
@@ -29,40 +25,6 @@ gitpush = "git push --set-upstream origin master"
 os.system(gitinit)
 os.system(gitadd)
 os.system(gitcommit)
-print(github_create_repo)
+os.system(github_create_repo)
 print(github_add_remote)
 print(gitpush)
-
-
-
-
-# cmd2 = 'remote set-url origin git@github.com:${USER:-${GITHUBUSER}}/${REPONAME:-${CURRENTDIR}}.git'
-# subprocess.call(['touch ', r, '.html'], shell = True)
-
-
-# subprocess.Popen('touch %u.html', shell = True)
-
-# print(r)
-# # print(u)
-
-# def subprocess_cmd(command):
-#     git add. 
-#     git -m 's'
-
-# subprocess_cmd(cmd1, cmd2, cmd3, )
-
-# subprocess_cmd('git remote set-url orgiin git@github.com%u/ add ; git commit -m \'initial commit\' ; )
-
-
-
-# CURRENTDIR=${PWD##*/}
-# GITHUBUSER=$(git config github.user)
-# 
-# git remote set-url origin git@github.com:${USER:-${GITHUBUSER}}/${REPONAME:-${CURRENTDIR}}.git
-# git add .
-# git commit -m 'initial commit'
-# git push --set-upstream origin master
-# }
-
-# executable with python gitcreate.py
-# create alias
