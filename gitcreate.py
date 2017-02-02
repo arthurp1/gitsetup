5dfbcc4545b74705018777e6a3d1fa1a7baba97f
arthurp1 <arthur.poot1@gmail.com>
2017-02-02 17:18:37 +0100

    error: src refspec master does not match any, probably needs delays

--- gitcreate.py
+++ gitcreate.py
@@ -18,9 +18,9 @@ user = removeNewline(subprocess.check_output('git config user.name', shell = Tru
 gitinit = "git init"
 gitadd = "git add ."
 gitcommit = "git commit -m 'initial commit'"
-github_create_repo = "curl -u {0} https://api.github.com/user/repos -d \'{{\"name\" : \"{1}\"}}\' --silent".format(user, repo)
-github_add_remote = "git remote add origin git@github.com:{0}/{1}.git --silent".format(user, repo)
-gitpush = "git push --set-upstream origin master --silent"
+github_create_repo = "curl -u {0} https://api.github.com/user/repos -d \'{{\"name\" : \"{1}\"}}\'".format(user, repo)
+github_add_remote = "git remote add origin git@github.com:{0}/{1}.git".format(user, repo)
+gitpush = "git push --set-upstream origin master"
 
 os.system(gitinit)
 os.system(gitadd)
